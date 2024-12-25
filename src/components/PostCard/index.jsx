d97887a20ee777e3a3dbd04a02053d20185302b5
Marcello Prado <marcellopradomuller@gmail.com>
2024-12-25 07:12:08 -0300

    colocando as / heheh

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -9,7 +9,7 @@ export default function PostCard({ post }) {
             <div className={styles.post}>
                 <img
                     className={styles.capa}
-                    src={`assets/posts/${post.id}/capa.png`}
+                    src={`/assets/posts/${post.id}/capa.png`}
                     alt='Imagem de capa do post'
                 />
                 <h2 className={styles.titulo}>{post.titulo}</h2>

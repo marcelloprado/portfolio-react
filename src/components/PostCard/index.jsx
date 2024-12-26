f3da3db7993e595b738e68fa662b155394beb989
Marcello Prado <marcellopradomuller@gmail.com>
2024-12-26 06:55:32 -0300

    Vamos cara!!

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,22 +1,21 @@
-import { Link } from "react-router-dom"
-import styles from "./PostCard.module.css"
-import BotaoPrincipal from "components/BotaoPrincipal"
+import { Link } from "react-router-dom";
+import styles from "./PostCard.module.css";
+import BotaoPrincipal from "components/BotaoPrincipal";
 
 export default function PostCard({ post }) {
-    return (
+  return (
+    <Link to={`/posts/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          className={styles.capa}
+          src={`/assets/posts/${post.id}/capa.png?v=${Date.now()}`}
+          alt="Imagem de capa do post"
+        />
+        <h2 className={styles.titulo}>{post.titulo}</h2>
+        <p className={styles.texto}>{post.texto}</p>
 
-        <Link to={`/posts/${post.id}`}>
-            <div className={styles.post}>
-                <img
-                    className={styles.capa}
-                    src={`/assets/posts/${post.id}/capa.png`}
-                    alt='Imagem de capa do post'
-                />
-                <h2 className={styles.titulo}>{post.titulo}</h2>
-                <p className={styles.texto}>{post.texto}</p>
-
-                <BotaoPrincipal>entrar</BotaoPrincipal>
-            </div>
-        </Link>
-    )
+        <BotaoPrincipal>entrar</BotaoPrincipal>
+      </div>
+    </Link>
+  );
 }

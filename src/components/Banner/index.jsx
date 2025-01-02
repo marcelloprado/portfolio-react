2e06d0f5bb0303d5deefd41dcc64df67958ac23c
Marcello Prado <marcellopradomuller@gmail.com>
2025-01-02 07:39:00 -0300

    escondendo o banner das demais páginas

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,42 +2,60 @@ import styles from "./Banner.module.css";
 import minhaFoto from "assets/minha_foto.png";
 import { TypeAnimation } from "react-type-animation";
 
+import { useState, useEffect, useCallback } from "react";
+
 export default function Banner() {
+    const [showBanner, setShowBanner] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0); // Armazenar a última posição do scroll
+
+    // Função para esconder ou mostrar o banner dependendo da direção do scroll
+    const handleScroll = useCallback(() => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > 20 && currentScrollY > lastScrollY) {
+            setShowBanner(false);
+        } else if (currentScrollY < lastScrollY || currentScrollY < 10) {
+            setShowBanner(true);
+        }
+
+        setLastScrollY(currentScrollY);
+    }, [lastScrollY]); // `lastScrollY` é uma dependência
+
+    // Adicionando o listener de scroll ao componente
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [handleScroll]); // Inclui `handleScroll` como dependência
+
     return (
-        <div className={styles.banner}>
+        showBanner && (
+            <div className={styles.banner}>
+                <div>
+                    <h1 className={styles.titulo}>Marcello Prado Muller</h1>
 
-            <div>
-                <h1 className={styles.titulo}>
-                    Marcello Prado Muller
-                </h1>
+                    <TypeAnimation
+                        className={styles.paragrafo}
+                        sequence={[
+                            `\nSeja Bem vindo(a) ao meu portfólio,\nEsse projeto foi desenvolvido com React Js\ne foi utilizado o React Router Dom, Rotas aninhadas\ne dinâmicas, Trabalhando com useLocation, NavLink, useStates, useNavigate e a famosa page not found 404, Abaixo segue alguns dos projetos. \nNo momento estou focado em desenvolvimento web com python e SQL.`,
+                            4000,
+                            "",
+                        ]}
+                        omitDeletionAnimation={true}
+                        repeat={Infinity}
+                    />
+                </div>
 
-                <TypeAnimation className={styles.paragrafo}
-                    // style={{ fontSize: '1.3rem', whiteSpace: 'pre-line', height: '50px', display: 'inline-block'}}
-                    sequence={[
-
-                        `\nSeja Bem vindo(a) ao meu portfólio,\nEsse projeto foi desenvolvido com React Js\ne foi utilizado o React Router Dom, Rotas aninhadas\ne dinâmicas, Trabalhando com useLocation, NavLink, useStates, useNavigate e a famosa page not found 404, Abaixo segue alguns dos projetos. \nNo momento estou focado em desenvolvimento web com python e SQL.`,
-                        4000,
-                        '',
-
-                    ]}
-                    omitDeletionAnimation={true}
-                    repeat={Infinity}
-                />
-
+                <div className={styles.imagens}>
+                    <img
+                        className={styles.minhaFoto}
+                        src={minhaFoto}
+                        alt="Foto do Marcello sorrindo"
+                    />
+                </div>
             </div>
-
-            <div className={styles.imagens}>
-                <img
-                    className={styles.minhaFoto}
-                    src={minhaFoto}
-                    alt="Foto do Marcello sorrindo"
-                />
-
-            </div>
-
-        </div>
-
-    )
+        )
+    );
 }
-
-
